9b7ce0322110eae36520bf2065a0ecaf7e4145b7
TheVinBear <TheVinBear@users.noreply.github.com>
2015-08-24 14:39:18 -0400

    Simple JunglerCalculator
    Version of JunglerCalculator file which creates 1 monster with full hp of jungle, and a champion(Warwick). Champ attacks monster until the monster is dead.

--- JunglerCalculator.java
+++ JunglerCalculator.java
@@ -15,32 +15,15 @@ public class JunglerCalculator
 {
 	public static void main(String[] args) 
 	{
-		//Spawn a champion, for test cases, Xin Zhao.
-		Champion player1 = initializeChampion("XinZhao");
+		//Spawn a champion, for test cases, Warwick.
+		Champion player1 = spawnChampion("Warwick");
 		
-		//Initialize the jungle monsters.
-		Monster miniWolf1 = new Monster("MurkWolf" , 0);
-		Monster miniWolf2 = new Monster("MurkWolf" , 0);
-		Monster bigWolf = new Monster("GreaterMurkWolf" , 0);
-		Monster toad= new Monster("Gromp" , 0);
-		Monster miniBlue1 = new Monster("Sentry" , 0);
-		Monster miniBlue2 = new Monster("Sentry" , 0);
-		Monster blueBuff = new Monster("BlueSentinel" , 0);
-		Monster miniRed1 = new Monster("Cinderling" , 0);
-		Monster miniRed2 = new Monster("Cinderling" , 0);
-		Monster redBuff = new Monster("RedBrambleback" , 0);
-		Monster miniRaptor1= new Monster("Raptor" , 0);
-		Monster miniRaptor2 = new Monster("Raptor" , 0);
-		Monster miniRaptor3 = new Monster("Raptor" , 0);
-		Monster bigRaptor = new Monster("CrimsonRaptor" , 0);
-		Monster miniKrug = new Monster("Krug" , 0);
-		Monster bigKrug = new Monster("AncientKrug", 0);
-		Monster crab = new Monster("ScuttleCrab" , 0);
-		
+		//Spawn a monster with HP of full jungle.
+		Monster target = spawnMonster("hpMonster", 0);
+
 		//Champion attacks the jungle until it is dead. Keeps count of how many attacks are performed.
-		
 		//Initialize the total attack counter, global timer, and attack timers.
-		//Need to rethink how I'll handle the timers.
+		//Need to rethink how I'll handle the timers, look into threads.
 		int attackCounter = 0;
 		double globalTimer = 115;
 		double attackTimer = 0;
@@ -52,33 +35,15 @@ public class JunglerCalculator
 			globalTimer += 0.001;
 			attackTimer += 0.001;
 			
-			player1.autoattack(toad);
-			player1.autoattack(blueBuff);
-			player1.autoattack(miniBlue1);
-			player1.autoattack(miniBlue2);
-			player1.autoattack(bigWolf);
-			player1.autoattack(miniWolf1);
-			player1.autoattack(miniWolf2);
-			player1.autoattack(redBuff);
-			player1.autoattack(miniRed1);
-			player1.autoattack(miniRed2);
-			player1.autoattack(miniRaptor1);
-			player1.autoattack(miniRaptor2);
-			player1.autoattack(miniRaptor3);
-			player1.autoattack(bigRaptor);
-			player1.autoattack(crab);
-			player1.autoattack(miniKrug);
-			player1.autoattack(bigKrug);
-			
 			//Makes the Champion attack if the attack cooldown is over.
 			if(attackTimer >= player1.atkSpd)
 			{
 				//Champion performs an attack to decrement the target's HP.
-				player1.autoattack();
+				player1.autoattack(target);
 				//Increment the attack counter.
 				attackCounter++;
 				//Displays the Current simulation time, current attack counter, and target HP.
-				System.out.println("Time: " + displayTime(globalTimer) + ". Attack: " + attackCounter + ". EnemyHP: " + jungle.hp);
+				System.out.println("Time: " + displayTime(globalTimer) + ". Attack: " + attackCounter + ". EnemyHP: " + target.hp);
 				//Resets the attack timer.
 				attackTimer = 0;
 			}
@@ -87,22 +52,16 @@ public class JunglerCalculator
 		
 	}
 	
-	
 	//Take the Champion name, and call the constructor to set the base statistics of the champion.
-	static Champion initializeChampion(String champName)
+	static Champion spawnChampion(String champName)
 	{
-		Champion spawnedChampion = new Champion(champName);
-		return spawnedChampion;
+		Champion newChampion = new Champion(champName);
+		return newChampion;
 	}
 	//Take the Monster name, and call the constructor to set the base statistics of the monster.
-	static void initializeJungle()
+	static Monster spawnMonster(String mobName, double timer)
 	{
-		
-		
-	}
-	static Monster summonMonster(String mobName)
-	{
-		Monster newMonster = new Monster(mobName, 0);
+		Monster newMonster = new Monster(mobName, timer);
 		return newMonster;
 	}
 	//Take a global timer of type Double, and converts it to a digital clock format.
